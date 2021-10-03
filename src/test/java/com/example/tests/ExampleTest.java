178a6e29d1b6274612f7e54be42523b00367151d
Mark Mullan <mark.mullan@clover.com>
2021-10-03 13:10:38 -0700

    test pushing some ugly code

--- src/test/java/com/example/tests/ExampleTest.java
+++ src/test/java/com/example/tests/ExampleTest.java
@@ -6,11 +6,12 @@ import org.junit.jupiter.api.Test;
 public class ExampleTest {
   @Test
   public void testExample() {
-    Assertions.assertEquals(11, "hello world".length());
+            Assertions.assertEquals(11, "hello world".length());
+String str=     "this is some bad looking java";
   }
 
   @Test
   public void testFailingTest() {
-    Assertions.assertTrue(1 > 2); // fixme!
+    Assertions.assertTrue(1 < 2); // fixme!
   }
 }
