4749b1184c9c258fc4528698f0c34a9fdc84d5f6
Mark Mullan <markrmullan@gmail.com>
2022-01-03 00:51:00 -0800

    fix inline shell script for closeAndReleaseRepository step

--- test.sh
+++ test.sh
@@ -1,17 +1,7 @@
 RELEASE_TAG_NAME="1.0.0-SNAPSHOT"
 
-#if [[ $(./gradlew -q printVersion) != "${RELEASE_TAG_NAME}" ]]
-#  then
-#    echo "Release tag (${RELEASE_TAG_NAME}) does not match the build.gradle version"
-#    exit 1
-#fi
-
-if [ 1 ]
+if [[ $(./gradlew -q printVersion) != "${RELEASE_TAG_NAME}" ]]
   then
-  echo << EOF
-    Publish to Nexus Sonatype succeeded but failed to close and release. This step assumes there
-    is only one staging repository active when it is called. Stale staging repositories may need
-    to be purged manually before proceeding. They can be removed by logging into
-    https://s01.oss.sonatype.org/.
-  EOF
+    echo "Release tag (${RELEASE_TAG_NAME}) does not match the build.gradle version"
+    exit 1
 fi
